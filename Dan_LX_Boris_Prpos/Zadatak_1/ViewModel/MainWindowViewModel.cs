8620d690aaf19986268373f4569096923d2ae4ed
BorisP1996 <prposboris13@hotmail.com>
2020-09-04 13:42:45 +0200

    with comments

--- Dan_LX_Boris_Prpos/Zadatak_1/ViewModel/MainWindowViewModel.cs
+++ Dan_LX_Boris_Prpos/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Zadatak_1.Command;
